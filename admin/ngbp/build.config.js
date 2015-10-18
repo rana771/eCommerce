e0b88a0e5938e5e2c77ad4c896a0821a4ada9729
Raihan <raihan.dk@gmail.com>
2015-10-18 18:49:20 +0600

    Build directory fixed

--- admin/ngbp/build.config.js
+++ admin/ngbp/build.config.js
@@ -7,7 +7,7 @@ module.exports = {
    * development and the `compile_dir` folder is where our app resides once it's
    * completely built.
    */
-  build_dir: '../',
+  build_dir: '..',
   compile_dir: 'bin',
 
   /**
@@ -72,5 +72,5 @@ module.exports = {
     ],
     assets: [
     ]
-  },
+  }
 };
